c037351e1e9ab73dfae1e9d04f8ffe52c221223a
Anjeliaaa <122423125+anjeliadwichy@users.noreply.github.com>
2025-06-19 10:23:10 +0700

    Update ModelJenisLayanan.cs

--- Model/ModelJenisLayanan.cs
+++ Model/ModelJenisLayanan.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +8,17 @@ namespace projectakhirsem2.Model
 {
     public class ModelJenisLayanan
     {
-        public int id_jenis_layanan { get; private set; }
+        public int id_jenis_layanan { get; protected set; }
         public string nama_jenis { get; set; }
 
         public ModelJenisLayanan(int id_jenis_layanan, string nama_jenis)
         {
             this.id_jenis_layanan = id_jenis_layanan;
             this.nama_jenis = nama_jenis;
+        }
+        public virtual string GetDisplayInfo()
+        {
+            return nama_jenis;
         }
     }
 }
