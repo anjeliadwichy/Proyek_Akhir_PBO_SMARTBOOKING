83e3c1333bf4d85dddedc436ff1035bade89af5b
Anjeliaaa <122423125+anjeliadwichy@users.noreply.github.com>
2025-06-19 10:24:47 +0700

    Update ModelPembayaran.cs

--- Model/ModelPembayaran.cs
+++ Model/ModelPembayaran.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace projectakhirsem2.Model
 {
-    public class ModelPembayaran
+    public class ModelPembayaran : ModelBookingRelated
     {
         public int id_pembayaran { get; set; }
         public int total_harga { get; set; }
         public DateTime tanggal_pembayaran { get; set; }
         public int id_metode_pembayaran { get; set; }
-        public int id_booking { get; set; }
-
         public ModelPembayaran(int id_pembayaran, int total_harga, DateTime tanggal_pembayaran, int id_metode_pembayaran, int id_booking)
+            : base(id_booking)
         {
             this.id_pembayaran = id_pembayaran;
             this.total_harga = total_harga;
             this.tanggal_pembayaran = tanggal_pembayaran;
             this.id_metode_pembayaran = id_metode_pembayaran;
-            this.id_booking = id_booking;
         }
     }
 }
