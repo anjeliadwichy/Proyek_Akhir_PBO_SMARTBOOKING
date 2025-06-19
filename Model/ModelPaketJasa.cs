c27d57c8b1ce36d32b2704908664269ffbe82077
Anjeliaaa <122423125+anjeliadwichy@users.noreply.github.com>
2025-06-19 10:24:09 +0700

    Update ModelPaketJasa.cs

--- Model/ModelPaketJasa.cs
+++ Model/ModelPaketJasa.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,55 +8,26 @@ namespace projectakhirsem2.Model
 {
     public class ModelLayananBase
     {
-        public int Id { get; protected set; }
-        public string Nama { get; set; }
-
-        public ModelLayananBase(int id, string nama)
+        public class ModelPaketJasa : ModelJenisLayanan
         {
-            Id = id;
-            Nama = nama;
-        }
+            public int id_paket_jasa { get; set; }
+            public string nama_paket { get; set; }
+            public int harga { get; set; }
+            public string deskripsi { get; set; }
 
-        public virtual string GetDisplayInfo()
-        {
-            return Nama;
-        }
-    }
+            public ModelPaketJasa(int id_paket_jasa, string nama_paket, int harga, string deskripsi, int id_jenis_layanan, string nama_jenis)
+                : base(id_jenis_layanan, nama_jenis)
+            {
+                this.id_paket_jasa = id_paket_jasa;
+                this.nama_paket = nama_paket;
+                this.harga = harga;
+                this.deskripsi = deskripsi;
+            }
 
-    public class ModelPaketJasa : ModelLayananBase
-    {
-        public int id_jenis_layanan { get; set; }
-        public int harga { get; set; }
-        public string deskripsi { get; set; }
-
-        public string nama_paket
-        {
-            get { return Nama; }  // dari base
-            set { Nama = value; }
-        }
-
-        public int id_paket_jasa
-        {
-            get { return Id; }    // dari base
-            private set { Id = value; }
-        }
-
-        public string DisplayInfo
-        {
-            get { return $"{nama_paket} - Rp{harga:N0}"; }
-        }
-
-        public ModelPaketJasa(int id_paket_jasa, string nama_paket, int id_jenis_layanan, int harga, string deskripsi)
-            : base(id_paket_jasa, nama_paket)
-        {
-            this.id_jenis_layanan = id_jenis_layanan;
-            this.harga = harga;
-            this.deskripsi = deskripsi;
-        }
-
-        public override string GetDisplayInfo()
-        {
-            return $"{nama_paket} - Rp{harga:N0}";
+            public override string GetDisplayInfo()
+            {
+                return $"{nama_paket} - Rp{harga:N0}";
+            }
         }
     }
 }
